e71be41f705caa99984348e1fdbdfa97064afbe5
Will Ehman <will@ehman.com>
2019-07-06 21:35:32 -0600

    project moved over to clion for better IDE

---
+++ clion/cmake-build-debug/CMakeFiles/clion.elf.dir/DependInfo.cmake
@@ -0,0 +1,78 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "ASM"
+  "C"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_ASM
+  "C:/Users/will/Documents/GitHub/STM32_Dev/clion/startup/startup_stm32l476xx.s" "C:/Users/will/Documents/GitHub/STM32_Dev/clion/cmake-build-debug/CMakeFiles/clion.elf.dir/startup/startup_stm32l476xx.s.obj"
+  )
+set(CMAKE_ASM_COMPILER_ID "GNU")
+
+# Preprocessor definitions for this target.
+set(CMAKE_TARGET_DEFINITIONS_ASM
+  "STM32L476xx"
+  "USE_HAL_DRIVER"
+  "__packed=__attribute__((__packed__))"
+  "__weak=__attribute__((weak))"
+  )
+
+# The include file search paths:
+set(CMAKE_ASM_TARGET_INCLUDE_PATH
+  "../Inc"
+  "../Drivers/STM32L4xx_HAL_Driver/Inc"
+  "../Drivers/STM32L4xx_HAL_Driver/Inc/Legacy"
+  "../Drivers/CMSIS/Device/ST/STM32L4xx/Include"
+  "../Drivers/CMSIS/Include"
+  )
+set(CMAKE_DEPENDS_CHECK_C
+  "C:/Users/will/Documents/GitHub/STM32_Dev/clion/Drivers/STM32L4xx_HAL_Driver/Src/stm32l4xx_hal.c" "C:/Users/will/Documents/GitHub/STM32_Dev/clion/cmake-build-debug/CMakeFiles/clion.elf.dir/Drivers/STM32L4xx_HAL_Driver/Src/stm32l4xx_hal.c.obj"
+  "C:/Users/will/Documents/GitHub/STM32_Dev/clion/Drivers/STM32L4xx_HAL_Driver/Src/stm32l4xx_hal_adc.c" "C:/Users/will/Documents/GitHub/STM32_Dev/clion/cmake-build-debug/CMakeFiles/clion.elf.dir/Drivers/STM32L4xx_HAL_Driver/Src/stm32l4xx_hal_adc.c.obj"
+  "C:/Users/will/Documents/GitHub/STM32_Dev/clion/Drivers/STM32L4xx_HAL_Driver/Src/stm32l4xx_hal_adc_ex.c" "C:/Users/will/Documents/GitHub/STM32_Dev/clion/cmake-build-debug/CMakeFiles/clion.elf.dir/Drivers/STM32L4xx_HAL_Driver/Src/stm32l4xx_hal_adc_ex.c.obj"
+  "C:/Users/will/Documents/GitHub/STM32_Dev/clion/Drivers/STM32L4xx_HAL_Driver/Src/stm32l4xx_hal_cortex.c" "C:/Users/will/Documents/GitHub/STM32_Dev/clion/cmake-build-debug/CMakeFiles/clion.elf.dir/Drivers/STM32L4xx_HAL_Driver/Src/stm32l4xx_hal_cortex.c.obj"
+  "C:/Users/will/Documents/GitHub/STM32_Dev/clion/Drivers/STM32L4xx_HAL_Driver/Src/stm32l4xx_hal_dma.c" "C:/Users/will/Documents/GitHub/STM32_Dev/clion/cmake-build-debug/CMakeFiles/clion.elf.dir/Drivers/STM32L4xx_HAL_Driver/Src/stm32l4xx_hal_dma.c.obj"
+  "C:/Users/will/Documents/GitHub/STM32_Dev/clion/Drivers/STM32L4xx_HAL_Driver/Src/stm32l4xx_hal_dma_ex.c" "C:/Users/will/Documents/GitHub/STM32_Dev/clion/cmake-build-debug/CMakeFiles/clion.elf.dir/Drivers/STM32L4xx_HAL_Driver/Src/stm32l4xx_hal_dma_ex.c.obj"
+  "C:/Users/will/Documents/GitHub/STM32_Dev/clion/Drivers/STM32L4xx_HAL_Driver/Src/stm32l4xx_hal_exti.c" "C:/Users/will/Documents/GitHub/STM32_Dev/clion/cmake-build-debug/CMakeFiles/clion.elf.dir/Drivers/STM32L4xx_HAL_Driver/Src/stm32l4xx_hal_exti.c.obj"
+  "C:/Users/will/Documents/GitHub/STM32_Dev/clion/Drivers/STM32L4xx_HAL_Driver/Src/stm32l4xx_hal_flash.c" "C:/Users/will/Documents/GitHub/STM32_Dev/clion/cmake-build-debug/CMakeFiles/clion.elf.dir/Drivers/STM32L4xx_HAL_Driver/Src/stm32l4xx_hal_flash.c.obj"
+  "C:/Users/will/Documents/GitHub/STM32_Dev/clion/Drivers/STM32L4xx_HAL_Driver/Src/stm32l4xx_hal_flash_ex.c" "C:/Users/will/Documents/GitHub/STM32_Dev/clion/cmake-build-debug/CMakeFiles/clion.elf.dir/Drivers/STM32L4xx_HAL_Driver/Src/stm32l4xx_hal_flash_ex.c.obj"
+  "C:/Users/will/Documents/GitHub/STM32_Dev/clion/Drivers/STM32L4xx_HAL_Driver/Src/stm32l4xx_hal_flash_ramfunc.c" "C:/Users/will/Documents/GitHub/STM32_Dev/clion/cmake-build-debug/CMakeFiles/clion.elf.dir/Drivers/STM32L4xx_HAL_Driver/Src/stm32l4xx_hal_flash_ramfunc.c.obj"
+  "C:/Users/will/Documents/GitHub/STM32_Dev/clion/Drivers/STM32L4xx_HAL_Driver/Src/stm32l4xx_hal_gpio.c" "C:/Users/will/Documents/GitHub/STM32_Dev/clion/cmake-build-debug/CMakeFiles/clion.elf.dir/Drivers/STM32L4xx_HAL_Driver/Src/stm32l4xx_hal_gpio.c.obj"
+  "C:/Users/will/Documents/GitHub/STM32_Dev/clion/Drivers/STM32L4xx_HAL_Driver/Src/stm32l4xx_hal_i2c.c" "C:/Users/will/Documents/GitHub/STM32_Dev/clion/cmake-build-debug/CMakeFiles/clion.elf.dir/Drivers/STM32L4xx_HAL_Driver/Src/stm32l4xx_hal_i2c.c.obj"
+  "C:/Users/will/Documents/GitHub/STM32_Dev/clion/Drivers/STM32L4xx_HAL_Driver/Src/stm32l4xx_hal_i2c_ex.c" "C:/Users/will/Documents/GitHub/STM32_Dev/clion/cmake-build-debug/CMakeFiles/clion.elf.dir/Drivers/STM32L4xx_HAL_Driver/Src/stm32l4xx_hal_i2c_ex.c.obj"
+  "C:/Users/will/Documents/GitHub/STM32_Dev/clion/Drivers/STM32L4xx_HAL_Driver/Src/stm32l4xx_hal_pwr.c" "C:/Users/will/Documents/GitHub/STM32_Dev/clion/cmake-build-debug/CMakeFiles/clion.elf.dir/Drivers/STM32L4xx_HAL_Driver/Src/stm32l4xx_hal_pwr.c.obj"
+  "C:/Users/will/Documents/GitHub/STM32_Dev/clion/Drivers/STM32L4xx_HAL_Driver/Src/stm32l4xx_hal_pwr_ex.c" "C:/Users/will/Documents/GitHub/STM32_Dev/clion/cmake-build-debug/CMakeFiles/clion.elf.dir/Drivers/STM32L4xx_HAL_Driver/Src/stm32l4xx_hal_pwr_ex.c.obj"
+  "C:/Users/will/Documents/GitHub/STM32_Dev/clion/Drivers/STM32L4xx_HAL_Driver/Src/stm32l4xx_hal_rcc.c" "C:/Users/will/Documents/GitHub/STM32_Dev/clion/cmake-build-debug/CMakeFiles/clion.elf.dir/Drivers/STM32L4xx_HAL_Driver/Src/stm32l4xx_hal_rcc.c.obj"
+  "C:/Users/will/Documents/GitHub/STM32_Dev/clion/Drivers/STM32L4xx_HAL_Driver/Src/stm32l4xx_hal_rcc_ex.c" "C:/Users/will/Documents/GitHub/STM32_Dev/clion/cmake-build-debug/CMakeFiles/clion.elf.dir/Drivers/STM32L4xx_HAL_Driver/Src/stm32l4xx_hal_rcc_ex.c.obj"
+  "C:/Users/will/Documents/GitHub/STM32_Dev/clion/Drivers/STM32L4xx_HAL_Driver/Src/stm32l4xx_hal_tim.c" "C:/Users/will/Documents/GitHub/STM32_Dev/clion/cmake-build-debug/CMakeFiles/clion.elf.dir/Drivers/STM32L4xx_HAL_Driver/Src/stm32l4xx_hal_tim.c.obj"
+  "C:/Users/will/Documents/GitHub/STM32_Dev/clion/Drivers/STM32L4xx_HAL_Driver/Src/stm32l4xx_hal_tim_ex.c" "C:/Users/will/Documents/GitHub/STM32_Dev/clion/cmake-build-debug/CMakeFiles/clion.elf.dir/Drivers/STM32L4xx_HAL_Driver/Src/stm32l4xx_hal_tim_ex.c.obj"
+  "C:/Users/will/Documents/GitHub/STM32_Dev/clion/Src/main.c" "C:/Users/will/Documents/GitHub/STM32_Dev/clion/cmake-build-debug/CMakeFiles/clion.elf.dir/Src/main.c.obj"
+  "C:/Users/will/Documents/GitHub/STM32_Dev/clion/Src/stm32l4xx_hal_msp.c" "C:/Users/will/Documents/GitHub/STM32_Dev/clion/cmake-build-debug/CMakeFiles/clion.elf.dir/Src/stm32l4xx_hal_msp.c.obj"
+  "C:/Users/will/Documents/GitHub/STM32_Dev/clion/Src/stm32l4xx_it.c" "C:/Users/will/Documents/GitHub/STM32_Dev/clion/cmake-build-debug/CMakeFiles/clion.elf.dir/Src/stm32l4xx_it.c.obj"
+  "C:/Users/will/Documents/GitHub/STM32_Dev/clion/Src/syscalls.c" "C:/Users/will/Documents/GitHub/STM32_Dev/clion/cmake-build-debug/CMakeFiles/clion.elf.dir/Src/syscalls.c.obj"
+  "C:/Users/will/Documents/GitHub/STM32_Dev/clion/Src/system_stm32l4xx.c" "C:/Users/will/Documents/GitHub/STM32_Dev/clion/cmake-build-debug/CMakeFiles/clion.elf.dir/Src/system_stm32l4xx.c.obj"
+  )
+set(CMAKE_C_COMPILER_ID "GNU")
+
+# Preprocessor definitions for this target.
+set(CMAKE_TARGET_DEFINITIONS_C
+  "STM32L476xx"
+  "USE_HAL_DRIVER"
+  "__packed=__attribute__((__packed__))"
+  "__weak=__attribute__((weak))"
+  )
+
+# The include file search paths:
+set(CMAKE_C_TARGET_INCLUDE_PATH
+  "../Inc"
+  "../Drivers/STM32L4xx_HAL_Driver/Inc"
+  "../Drivers/STM32L4xx_HAL_Driver/Inc/Legacy"
+  "../Drivers/CMSIS/Device/ST/STM32L4xx/Include"
+  "../Drivers/CMSIS/Include"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
