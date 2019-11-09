ce191c0e48ce8db62494ac0fa5578a7342561b74
Will Ehman <will@ehman.com>
2019-11-09 16:11:39 -0700

    updates

---
+++ disco_test/cmake-build-debug/CMakeFiles/disco_test.elf.dir/DependInfo.cmake
@@ -0,0 +1,74 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "ASM"
+  "C"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_ASM
+  "C:/Users/will/Documents/GitHub/STM32_Dev/disco_test/startup/startup_stm32f429xx.s" "C:/Users/will/Documents/GitHub/STM32_Dev/disco_test/cmake-build-debug/CMakeFiles/disco_test.elf.dir/startup/startup_stm32f429xx.s.obj"
+  )
+set(CMAKE_ASM_COMPILER_ID "GNU")
+
+# Preprocessor definitions for this target.
+set(CMAKE_TARGET_DEFINITIONS_ASM
+  "STM32F429xx"
+  "USE_HAL_DRIVER"
+  "__packed=__attribute__((__packed__))"
+  "__weak=__attribute__((weak))"
+  )
+
+# The include file search paths:
+set(CMAKE_ASM_TARGET_INCLUDE_PATH
+  "../Inc"
+  "../Drivers/STM32F4xx_HAL_Driver/Inc"
+  "../Drivers/STM32F4xx_HAL_Driver/Inc/Legacy"
+  "../Drivers/CMSIS/Device/ST/STM32F4xx/Include"
+  "../Drivers/CMSIS/Include"
+  )
+set(CMAKE_DEPENDS_CHECK_C
+  "C:/Users/will/Documents/GitHub/STM32_Dev/disco_test/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal.c" "C:/Users/will/Documents/GitHub/STM32_Dev/disco_test/cmake-build-debug/CMakeFiles/disco_test.elf.dir/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal.c.obj"
+  "C:/Users/will/Documents/GitHub/STM32_Dev/disco_test/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_cortex.c" "C:/Users/will/Documents/GitHub/STM32_Dev/disco_test/cmake-build-debug/CMakeFiles/disco_test.elf.dir/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_cortex.c.obj"
+  "C:/Users/will/Documents/GitHub/STM32_Dev/disco_test/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_dma.c" "C:/Users/will/Documents/GitHub/STM32_Dev/disco_test/cmake-build-debug/CMakeFiles/disco_test.elf.dir/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_dma.c.obj"
+  "C:/Users/will/Documents/GitHub/STM32_Dev/disco_test/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_dma_ex.c" "C:/Users/will/Documents/GitHub/STM32_Dev/disco_test/cmake-build-debug/CMakeFiles/disco_test.elf.dir/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_dma_ex.c.obj"
+  "C:/Users/will/Documents/GitHub/STM32_Dev/disco_test/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_exti.c" "C:/Users/will/Documents/GitHub/STM32_Dev/disco_test/cmake-build-debug/CMakeFiles/disco_test.elf.dir/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_exti.c.obj"
+  "C:/Users/will/Documents/GitHub/STM32_Dev/disco_test/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_flash.c" "C:/Users/will/Documents/GitHub/STM32_Dev/disco_test/cmake-build-debug/CMakeFiles/disco_test.elf.dir/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_flash.c.obj"
+  "C:/Users/will/Documents/GitHub/STM32_Dev/disco_test/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_flash_ex.c" "C:/Users/will/Documents/GitHub/STM32_Dev/disco_test/cmake-build-debug/CMakeFiles/disco_test.elf.dir/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_flash_ex.c.obj"
+  "C:/Users/will/Documents/GitHub/STM32_Dev/disco_test/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_flash_ramfunc.c" "C:/Users/will/Documents/GitHub/STM32_Dev/disco_test/cmake-build-debug/CMakeFiles/disco_test.elf.dir/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_flash_ramfunc.c.obj"
+  "C:/Users/will/Documents/GitHub/STM32_Dev/disco_test/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_gpio.c" "C:/Users/will/Documents/GitHub/STM32_Dev/disco_test/cmake-build-debug/CMakeFiles/disco_test.elf.dir/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_gpio.c.obj"
+  "C:/Users/will/Documents/GitHub/STM32_Dev/disco_test/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_pwr.c" "C:/Users/will/Documents/GitHub/STM32_Dev/disco_test/cmake-build-debug/CMakeFiles/disco_test.elf.dir/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_pwr.c.obj"
+  "C:/Users/will/Documents/GitHub/STM32_Dev/disco_test/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_pwr_ex.c" "C:/Users/will/Documents/GitHub/STM32_Dev/disco_test/cmake-build-debug/CMakeFiles/disco_test.elf.dir/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_pwr_ex.c.obj"
+  "C:/Users/will/Documents/GitHub/STM32_Dev/disco_test/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_rcc.c" "C:/Users/will/Documents/GitHub/STM32_Dev/disco_test/cmake-build-debug/CMakeFiles/disco_test.elf.dir/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_rcc.c.obj"
+  "C:/Users/will/Documents/GitHub/STM32_Dev/disco_test/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_rcc_ex.c" "C:/Users/will/Documents/GitHub/STM32_Dev/disco_test/cmake-build-debug/CMakeFiles/disco_test.elf.dir/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_rcc_ex.c.obj"
+  "C:/Users/will/Documents/GitHub/STM32_Dev/disco_test/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_tim.c" "C:/Users/will/Documents/GitHub/STM32_Dev/disco_test/cmake-build-debug/CMakeFiles/disco_test.elf.dir/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_tim.c.obj"
+  "C:/Users/will/Documents/GitHub/STM32_Dev/disco_test/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_tim_ex.c" "C:/Users/will/Documents/GitHub/STM32_Dev/disco_test/cmake-build-debug/CMakeFiles/disco_test.elf.dir/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_tim_ex.c.obj"
+  "C:/Users/will/Documents/GitHub/STM32_Dev/disco_test/Src/main.c" "C:/Users/will/Documents/GitHub/STM32_Dev/disco_test/cmake-build-debug/CMakeFiles/disco_test.elf.dir/Src/main.c.obj"
+  "C:/Users/will/Documents/GitHub/STM32_Dev/disco_test/Src/stm32f4xx_hal_msp.c" "C:/Users/will/Documents/GitHub/STM32_Dev/disco_test/cmake-build-debug/CMakeFiles/disco_test.elf.dir/Src/stm32f4xx_hal_msp.c.obj"
+  "C:/Users/will/Documents/GitHub/STM32_Dev/disco_test/Src/stm32f4xx_it.c" "C:/Users/will/Documents/GitHub/STM32_Dev/disco_test/cmake-build-debug/CMakeFiles/disco_test.elf.dir/Src/stm32f4xx_it.c.obj"
+  "C:/Users/will/Documents/GitHub/STM32_Dev/disco_test/Src/syscalls.c" "C:/Users/will/Documents/GitHub/STM32_Dev/disco_test/cmake-build-debug/CMakeFiles/disco_test.elf.dir/Src/syscalls.c.obj"
+  "C:/Users/will/Documents/GitHub/STM32_Dev/disco_test/Src/system_stm32f4xx.c" "C:/Users/will/Documents/GitHub/STM32_Dev/disco_test/cmake-build-debug/CMakeFiles/disco_test.elf.dir/Src/system_stm32f4xx.c.obj"
+  )
+set(CMAKE_C_COMPILER_ID "GNU")
+
+# Preprocessor definitions for this target.
+set(CMAKE_TARGET_DEFINITIONS_C
+  "STM32F429xx"
+  "USE_HAL_DRIVER"
+  "__packed=__attribute__((__packed__))"
+  "__weak=__attribute__((weak))"
+  )
+
+# The include file search paths:
+set(CMAKE_C_TARGET_INCLUDE_PATH
+  "../Inc"
+  "../Drivers/STM32F4xx_HAL_Driver/Inc"
+  "../Drivers/STM32F4xx_HAL_Driver/Inc/Legacy"
+  "../Drivers/CMSIS/Device/ST/STM32F4xx/Include"
+  "../Drivers/CMSIS/Include"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ disco_test/cmake-build-debug/CMakeFiles/disco_test.elf.dir/cmake_clean.cmake
@@ -0,0 +1,31 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/disco_test.elf.dir/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal.c.obj"
+  "CMakeFiles/disco_test.elf.dir/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_cortex.c.obj"
+  "CMakeFiles/disco_test.elf.dir/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_dma.c.obj"
+  "CMakeFiles/disco_test.elf.dir/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_dma_ex.c.obj"
+  "CMakeFiles/disco_test.elf.dir/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_exti.c.obj"
+  "CMakeFiles/disco_test.elf.dir/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_flash.c.obj"
+  "CMakeFiles/disco_test.elf.dir/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_flash_ex.c.obj"
+  "CMakeFiles/disco_test.elf.dir/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_flash_ramfunc.c.obj"
+  "CMakeFiles/disco_test.elf.dir/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_gpio.c.obj"
+  "CMakeFiles/disco_test.elf.dir/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_pwr.c.obj"
+  "CMakeFiles/disco_test.elf.dir/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_pwr_ex.c.obj"
+  "CMakeFiles/disco_test.elf.dir/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_rcc.c.obj"
+  "CMakeFiles/disco_test.elf.dir/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_rcc_ex.c.obj"
+  "CMakeFiles/disco_test.elf.dir/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_tim.c.obj"
+  "CMakeFiles/disco_test.elf.dir/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_tim_ex.c.obj"
+  "CMakeFiles/disco_test.elf.dir/Src/main.c.obj"
+  "CMakeFiles/disco_test.elf.dir/Src/stm32f4xx_hal_msp.c.obj"
+  "CMakeFiles/disco_test.elf.dir/Src/stm32f4xx_it.c.obj"
+  "CMakeFiles/disco_test.elf.dir/Src/syscalls.c.obj"
+  "CMakeFiles/disco_test.elf.dir/Src/system_stm32f4xx.c.obj"
+  "CMakeFiles/disco_test.elf.dir/startup/startup_stm32f429xx.s.obj"
+  "disco_test.elf.pdb"
+  "disco_test.elf"
+  "disco_test.elf.manifest"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang ASM C)
+  include(CMakeFiles/disco_test.elf.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
